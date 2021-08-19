c74a0f9c048063a9e3500390dfe2f1faa63a448a
GHAYku <kusa07122@gmail.com>
2021-08-19 03:46:29 +0000

    [Finish]deviseの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_030854) do
+ActiveRecord::Schema.define(version: 2021_08_19_031854) do
 
   create_table "comentes", force: :cascade do |t|
     t.integer "post_id"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(version: 2021_08_19_030854) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "end_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.text "body"
+    t.string "image_id"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_end_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
