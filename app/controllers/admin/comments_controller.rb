baec5053023b577984b342458c431dfda23c80ff
GHAYku <kusa07122@gmail.com>
2021-09-15 07:11:35 +0000

    [Fix]最終調整

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,7 +2,7 @@ class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @comments = Comment.all.order(created_at: :desc).page(params[:page]).per(15)
+    @comments = Comment.all.order(created_at: :desc).order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def edit
