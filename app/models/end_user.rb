f5c80c5e4d9a7cb3ce4282094f237ddae2df9c5a
GHAYku <kusa07122@gmail.com>
2021-09-19 00:56:55 +0000

    [Fix]オーアース認証修正

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -38,7 +38,6 @@ class EndUser < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
 
   validates :name, presence: true, length: { maximum: 12 }
-  validates :email, presence: true
 
   def follow(end_user_id)
     relationships.create(followed_id: end_user_id)
